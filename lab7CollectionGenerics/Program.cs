4e628ed4157e38dcd7f3ba58334a4489104d6179
ai-dev-official <ai.codespro@gmail.com>
2023-11-15 07:56:00 +0000

    Added lab 7 revision

---
+++ lab7CollectionGenerics/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab7CollectionGenerics
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+    }
+
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+
+        public Student(string id, string name, Gender gender)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} Name: {Name} Gender: {Gender}";
+        }
+    }
+
+    public class StudentClass : IEnumerable
+    {
+        public string Crn { get; set; }
+        public string LecturerName { get; set; }
+        private List<Student> students;
+
+
+        public StudentClass(string crn, string lecturerName)
+        {
+            Crn = crn;
+            LecturerName = lecturerName;
+            students = new List<Student>();
+        }
+
+        public void addStudent(Student student)
+        {
+            if(students.Exists(s=>s.Id == student.Id))
+            {
+                throw new ArgumentException("Error: Student already exists");
+            }
+            else
+            {
+                students.Add(student);
+            }
+        }
+
+        public Student this[int i]
+        {
+            get
+            {
+                if(i >= 0 && i < students.Count)
+                {
+                    return students[i];
+                }
+                else
+                {
+                    throw new ArgumentException("Index out of range");
+                }
+            }
+        }
+
+        public Student this[string id]
+        {
+            get
+            {
+                Student student = students.FirstOrDefault(s => s.Id == id);
+                if(student != null)
+                {
+                    return student;
+                }
+                else
+                {
+                    throw new ArgumentException("Student cannot be null");
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach(Student s in students)
+            {
+                yield return s;
+            }
+        }
+    }
+
+
+    
+
+
+
+
+
+ 
+}
---
+++ lab7CollectionGenerics/ProgramMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7CollectionGenerics
+{
+    static class ProgramMain
+    {
+
+        static void Main()
+        {
+            try
+            {
+                Student S1 = new Student("x00", "John", Gender.Male);
+                Student S2 = new Student("x01", "Jane", Gender.Female);
+                Student S3 = new Student("x02", "Jessy", Gender.Male);
+
+                StudentClass SC1 = new StudentClass("oosdev", "Mike");
+
+                SC1.addStudent(S1);
+                SC1.addStudent(S2);
+                SC1.addStudent(S3);
+
+                foreach (Student student in SC1)
+                {
+                    Console.WriteLine(student);
+                }
+
+                Student s = SC1["x02"];
+                Console.WriteLine("Found: "+ s);
+                Student s2 = SC1[1];
+                Console.WriteLine("Found: " + s2);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+       
+    }
+}
